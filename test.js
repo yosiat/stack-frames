71598afa89a7b49deea5ff01eabed80360087429
Yosi Attias <yosy101@gmail.com>
2024-03-28 20:38:53 +0200

    Move to node:test

--- test.js
+++ test.js
@@ -1,32 +1,30 @@
 const stackFrames = require("./lib/binding");
-const test = require("ava");
+const test = require('node:test');
+const assert = require('node:assert');
 
-console.log(__filename);
 
-test("returns file name", (t) => {
-  t.is(stackFrames.getAt(0).file_name, __filename);
+test("returns file name", () => {
+  assert.equal(stackFrames.getAt(0).file_name, __filename);
 });
 
-test("nested", (t) => {
+test("nested", () => {
   const g = () => stackFrames.getAt(0);
   const f = () => g();
 
-  t.deepEqual(f(), {
+  assert.deepEqual(f(), {
     line_number: 11, // where `g` is declared
     file_name: __filename,
   });
 });
 
-test("missing stack frame index parameter", (t) => {
-  const error = t.throws(() => stackFrames.getAt());
-  t.is(error.message, "Missing stack frame index parameter");
+test("missing stack frame index parameter", () => {
+  assert.throws(() => stackFrames.getAt(), { message: "Missing stack frame index parameter" });
 });
 
-test("stack frame index parameter is not number", (t) => {
-  const error = t.throws(() => stackFrames.getAt("hello"));
-  t.is(error.message, "Stack frame index parameter is not a number");
+test("stack frame index parameter is not number", () => {
+  assert.throws(() => stackFrames.getAt("hello"), { message: "Stack frame index parameter is not a number" });
 });
 
-test("not found stack frame index returns null", (t) => {
-  t.is(stackFrames.getAt(34578), null);
+test("not found stack frame index returns null", () => {
+  assert.equal(stackFrames.getAt(34578), null);
 });
